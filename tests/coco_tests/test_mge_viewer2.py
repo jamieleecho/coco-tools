a3127c52f10714d96b8413793c5b6c7750548fa2
Jamie Cho <jamieleecho@gmail.com>
2025-03-30 19:55:40 -0700

    Migrate build system to use uv

--- tests/coco_tests/test_mge_viewer2.py
+++ tests/coco_tests/test_mge_viewer2.py
@@ -33,6 +33,7 @@ class TestMGE_Viewer2(unittest.TestCase):
                 env={"PYTHONPATH": "."},
                 stderr=subprocess.STDOUT,
             )
+
         self.assertRegex(iotostr(context.exception.output), self.USAGE_REGEX)
         self.assertRegex(
             iotostr(context.exception.output),
